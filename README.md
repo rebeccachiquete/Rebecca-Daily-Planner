9cbf006eb04c7d1a4ca1dc1ac7cdd53611c62743
Rebecca Chiquete <deerinra194@gmail.com>
2020-12-12 11:11:38 -0700

    finalpush

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Rebecca-Daily-Planner
+
+
+HTML was super easy, we went over creating it in class. Just had to create one line and then copy and change the hours. 
+
+I had a tough time i put the JS file above the jquery and it was giving me an error. Finally figured that out.
+
+functions are getting easier with time. This was was a little tricky because I forgot about the JSON.parse but a classmate reminded me and that made it click in my mind. 
+
+my page was not saving the "Tasks" and it was frustracting me. Then I realized on line 66 in JS i had it written without a capital and when i fixed that it worked. 
+
+Tutor went over arrays and Jquery with me on wednesday so that realllllly helped. 
+
+It was nice having CSS already created. 
---
+++ script.js
@@ -0,0 +1,71 @@
+// create total function
+$(document).ready(function(){
+
+    // make current day 
+var currentDay = moment().format('MMM Do YYYY, h:mm:ss a');
+
+    // display current day
+$("#currentDay").text(currentDay)
+
+    // global variables
+var dayHours = moment().format('LT');
+var tables = $(".row");
+var hours = [1, 2, 3, 4, 5, 9, 10, 11, 12];
+var textArea = $("textarea");
+
+    // create loop
+for (var i = 0; i < hours.length; i++) {
+    var dayHours = parseInt(dayHours);
+    var htmlHours = hours[i];
+    var tableLoop = tables[i];
+
+    if (htmlHours === dayHours) {
+        $(tableLoop).addClass("present");
+    }
+    if (htmlHours < dayHours) {
+        $(tableLoop).addClass("past");
+    }
+    if (htmlHours > dayHours) {
+        $(tableLoop).addClass("future");
+    }
+}
+
+    // when refreshing page, tasks will remain
+var typedTasks = JSON.parse(localStorage.getItem("Tasks"));
+
+
+if (typedTasks !==null) {
+    $(textArea[0]).text(typedTasks.nine);
+    $(textArea[1]).text(typedTasks.ten);
+    $(textArea[2]).text(typedTasks.eleven);
+    $(textArea[3]).text(typedTasks.twelve);
+    $(textArea[4]).text(typedTasks.one);
+    $(textArea[5]).text(typedTasks.two);
+    $(textArea[6]).text(typedTasks.three);
+    $(textArea[7]).text(typedTasks.four);
+    $(textArea[8]).text(typedTasks.five);
+}
+
+    // save function and tasks when saving button is clicked
+function saveTypedTasks(event) {
+    event.preventDefault();
+
+    var tasks = {
+        nine: textArea[0].value.trim(),
+        ten: textArea[1].value.trim(),
+        eleven: textArea[2].value.trim(),
+        twelve: textArea[3].value.trim(),
+        one: textArea[4].value.trim(),
+        two: textArea[5].value.trim(),
+        three: textArea[6].value.trim(),
+        four: textArea[7].value.trim(),
+        five: textArea[8].value.trim(),
+    }
+
+    // store in logal storage
+    localStorage.setItem("Tasks", JSON.stringify(tasks));
+}
+    // finish function
+    $(".saveBtn").on("click", saveTypedTasks);
+
+})
